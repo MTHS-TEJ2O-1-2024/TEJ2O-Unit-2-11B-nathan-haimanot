27ad26506b40d518d3f6107c1e9158f80c3d585e
nathan-haimanot <nathan.haimanot@mths.ca>
2024-10-22 11:19:38 -0400

    added comments

--- main.ts
+++ main.ts
@@ -7,19 +7,21 @@
 
 let firstNumber = randint(0,99)
 let secondNumber = randint(0,99)
-
+// first Number
 input.onButtonPressed(Button.A, function(){
     basic.showString("#1:")
     basic.showNumber(firstNumber)
     basic.showIcon(IconNames.Happy)
     basic.clearScreen()
 })
+// second Numer
 input.onButtonPressed(Button.B, function(){
     basic.showString("#2:")
     basic.showNumber(secondNumber)
     basic.showIcon(IconNames.Happy)
     basic.clearScreen()
 })
+// comparing numbers
 input.onGesture(Gesture.Shake, function(){
     if (firstNumber > secondNumber){
         basic.showString(firstNumber + ">" + secondNumber)
