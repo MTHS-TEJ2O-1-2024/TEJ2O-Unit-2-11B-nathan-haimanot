577856daf33617dc655de4be457a7280e6699f3b
nathan-haimanot <nathan.haimanot@mths.ca>
2024-10-22 11:18:07 -0400

    made a code that compares numbers

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Nathan Haimanot
+ * Created on: Oct 2024
+ * This program compares numbers
 */
 
-basic.showString('Hello, World!')
+let firstNumber = randint(0,99)
+let secondNumber = randint(0,99)
+
+input.onButtonPressed(Button.A, function(){
+    basic.showString("#1:")
+    basic.showNumber(firstNumber)
+    basic.showIcon(IconNames.Happy)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function(){
+    basic.showString("#2:")
+    basic.showNumber(secondNumber)
+    basic.showIcon(IconNames.Happy)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function(){
+    if (firstNumber > secondNumber){
+        basic.showString(firstNumber + ">" + secondNumber)
+        } else {
+        basic.showString(firstNumber + "<" + secondNumber)
+        basic.showIcon(IconNames.Happy)
+}
+})
